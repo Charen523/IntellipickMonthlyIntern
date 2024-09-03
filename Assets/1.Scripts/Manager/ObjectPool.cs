70262577709a93ab2278ceb0c288185905a503e6
Chloe <144107013+Charen523@users.noreply.github.com>
2024-09-03 16:22:46 +0900

    [feat] ObjectPool 구현

--- Assets/1.Scripts/Manager/ObjectPool.cs
+++ Assets/1.Scripts/Manager/ObjectPool.cs
@@ -1,18 +1,57 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectPool : MonoBehaviour
+[Serializable]
+public class Pool
 {
-    // Start is called before the first frame update
-    void Start()
+    public string tag;
+    public GameObject prefab;
+    public int MinCount;
+    public int MaxCount;    
+}
+public class ObjectPool : Singleton<ObjectPool>
+{
+    public List<Pool> pools; 
+    private Dictionary<string, Queue<MonoBehaviour>> poolDictionary = new Dictionary<string, Queue<MonoBehaviour>>();
+    private Transform objectPoolParent;
+
+    private void Start()
     {
-        
+        objectPoolParent = new GameObject("ObjectPool").transform;
+
+        foreach (Pool pool in pools)
+        {
+            CreatePool(pool);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void CreatePool(Pool pool)
     {
-        
+        Queue<MonoBehaviour> objectPool = new Queue<MonoBehaviour>();
+        Transform tagParent = new GameObject(pool.tag).transform;
+        tagParent.SetParent(objectPoolParent);
+
+        for (int i = 0; i < pool.MinCount; i++)
+        {
+            GameObject obj = Instantiate(pool.prefab, tagParent);
+            MonoBehaviour objInstance = obj.GetComponent<MonoBehaviour>();
+            objInstance.gameObject.SetActive(false);
+            objectPool.Enqueue(objInstance);
+        }
+
+        poolDictionary.Add(pool.tag, objectPool);
+    }
+
+    public T Get<T>(string tag) where T : MonoBehaviour 
+    {
+        if (!poolDictionary.ContainsKey(tag)) return default;
+
+        MonoBehaviour obj;
+        obj = poolDictionary[tag].Dequeue();
+        poolDictionary[tag].Enqueue(obj);
+
+        obj.gameObject.SetActive(true);
+        return (T)obj;
     }
 }
