70ebdf1a5d82a8ab7380939a0b2c51fe52038108
Chloe <144107013+Charen523@users.noreply.github.com>
2024-09-04 13:08:12 +0900

    [feat] 몬스터 생성/사망 로직

--- Assets/1.Scripts/Manager/GameManager.cs
+++ Assets/1.Scripts/Manager/GameManager.cs
@@ -1,6 +1,29 @@
+using System;
 using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
 {
+    public event Action MonsterDead;
 
+    public readonly string arrowTag = "Arrow";
+    public readonly string monsterTag = "Monster";
+
+    public Monster curMonster;
+    public GameObject MonsterInfo;
+
+    private void Start()
+    {
+        MonsterDead += SpawnMonster;
+        SpawnMonster();
+    }
+
+    private void SpawnMonster()
+    {
+        curMonster = ObjectPool.Instance.Get<Monster>(monsterTag);
+    }
+
+    public void OnMonsterDead()
+    {
+        MonsterDead?.Invoke();
+    }
 }
