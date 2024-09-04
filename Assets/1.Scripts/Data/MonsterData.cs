c4cad4165f48d4251c3262bc971be547fe38c515
Chloe <144107013+Charen523@users.noreply.github.com>
2024-09-04 11:42:46 +0900

    [feat] MonsterData 변경 + BaseController 구성

--- Assets/1.Scripts/Data/MonsterData.cs
+++ Assets/1.Scripts/Data/MonsterData.cs
@@ -1,5 +1,4 @@
 using System;
-using UnityEngine;
 
 public interface IData
 {
@@ -9,10 +8,11 @@ public interface IData
 [Serializable]
 public class MonsterData : IData
 {
+    public int id;
     public string Name;
     public string Grade;
     public float Speed;
     public int Health;
 
-    public string Id => Name;
+    public string Id => id.ToString();
 }
