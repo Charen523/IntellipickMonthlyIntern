5a8185c254c4e5b9d22514811966da30c19af42d
Chloe <144107013+Charen523@users.noreply.github.com>
2024-09-04 13:07:36 +0900

    [feat] Arrow 설정

---
+++ Assets/1.Scripts/Entity/Arrow/Arrow.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb;
+
+    public int arrowDmg;
+    private Vector3 endPos;
+
+    private bool isShoot = false;
+
+    private void Update()
+    {
+        if (isShoot)
+        {
+            rb.velocity = Vector2.right;
+            if (transform.position == endPos)
+            {
+                ResetArrow();
+            }
+        }
+    }
+
+    public void GetAttackSign(Vector3 start, int damage)
+    {
+        isShoot = true;
+        rb.position = start;
+        endPos = new Vector3(10, start.y, 0);
+    }
+
+    private void ResetArrow()
+    {
+        isShoot = false;
+        transform.position = endPos;
+        rb.velocity = Vector3.zero;
+        gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Monster"))
+        {
+            GameManager.Instance.curMonster.OnDamage(arrowDmg);
+            ResetArrow();
+        }
+    }
+}
